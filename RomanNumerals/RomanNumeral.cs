6893e2e1dd77fa658aa3037ac499c15b061d1e1e
NyoronDev <jsernajaen@gmail.com>
2017-06-07 22:02:51 +0200

    Add project files.

---
+++ RomanNumerals/RomanNumeral.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumeral
+    {
+        private Dictionary<int, string> baseNumbers;
+
+        public RomanNumeral()
+        {
+            baseNumbers = new Dictionary<int, string>();
+            baseNumbers.Add(1, "I");
+            baseNumbers.Add(4, "IV");
+            baseNumbers.Add(5, "V");
+            baseNumbers.Add(9, "IX");
+            baseNumbers.Add(10, "X");
+            baseNumbers.Add(40, "XL");
+            baseNumbers.Add(50, "L");
+            baseNumbers.Add(90, "XC");
+            baseNumbers.Add(100, "C");
+            baseNumbers.Add(400, "CD");
+            baseNumbers.Add(500, "D");
+            baseNumbers.Add(900, "CM");
+            baseNumbers.Add(1000, "M");
+        }
+
+        public string GetRomanValueFromArabicNum(int number)
+        {
+            var currentNumber = number;
+            var ret = ""; 
+            var keys = baseNumbers.AsEnumerable().Reverse().ToArray();
+            var i = 0; 
+            while (currentNumber > 0)
+            {
+                var item = keys[i];
+                if(currentNumber >= item.Key)
+                {
+                    currentNumber -= item.Key;
+                    ret += item.Value;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return ret; 
+        }
+
+        public string GetRomanValueFromArabicNumKK(int number)
+        {
+            string  romanNumber;
+
+            int previous = 0;
+            int next = 0;
+            foreach (var item in baseNumbers)
+            {
+                next = item.Key;
+                if (item.Key == number)
+                {
+                    return item.Value;
+                }
+
+                if (number > item.Key)
+                {
+                    previous = item.Key;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var diff = number - previous;
+            if (diff < 3)
+            {
+                romanNumber = string.Join("", Enumerable.Repeat(baseNumbers[previous], number).ToArray());
+            }
+            else
+            {
+                romanNumber = baseNumbers[previous] + baseNumbers[next];
+            }
+
+            return romanNumber;
+        }
+    }
+}
---
+++ RomanNumeralsTest/RomanTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals;
+
+namespace RomanNumeralsTest
+{
+
+    [TestClass]
+    public class RomanTest
+    {
+        /// <summary>
+        /// Romans the numeral is not null.
+        /// </summary>
+        [TestMethod]
+        public void RomanNumeral_IsNotNull()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            // Assert
+            Assert.IsNotNull(romanNumeral);
+        }
+
+        /// <summary>
+        ///  
+        /// </summary>
+        [TestMethod]
+        public void RomanNumeral_GetNumber1FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(1);
+
+            // Assert
+            Assert.AreEqual("I", romanNumeralValue);
+        }
+
+        /// <summary>
+        ///  
+        /// </summary>
+        [TestMethod]
+        public void RomanNumeral_GetNumber3FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(3);
+
+            // Assert
+            Assert.AreEqual("III", romanNumeralValue);
+        }
+
+        [TestMethod]
+        public void RomanNumeral_GetNumber4FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(4);
+
+            // Assert
+            Assert.AreEqual("IV", romanNumeralValue);
+        }
+        [TestMethod]
+        public void RomanNumeral_GetNumber5FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(5);
+
+            // Assert
+            Assert.AreEqual("V", romanNumeralValue);
+        }
+
+        [TestMethod]
+        public void RomanNumeral_GetNumber2FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(2);
+
+            // Assert
+            Assert.AreEqual("II", romanNumeralValue);
+        }
+
+        [TestMethod]
+        public void RomanNumeral_GetNumber6FromArabic()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(6);
+
+            // Assert
+            Assert.AreEqual("VI", romanNumeralValue);
+        }
+
+        [TestMethod]
+        public void RomanNumeral_GetLast()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            var romanNumeralValue = romanNumeral.GetRomanValueFromArabicNum(3999);
+
+            // Assert
+            Assert.AreEqual("MMMCMXCIX", romanNumeralValue);
+        }
+    }
+}
